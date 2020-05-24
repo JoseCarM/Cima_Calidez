1c8d33670c085604f46a976d6d86390878bd46ec
José Carlos Cárdenas Martínez <Josecm@icloud.com>
2020-05-24 16:07:31 -0500

    Transiciones incluidas en los botones de index y refactorización de codigo js
    -Se incluyen transiciones en botones de navegación y de miniaturas.
    -Se reemplazan llamados de eventos en js por pseudo clases en css
    -Se simplifica el código js

--- eventosCima.js
+++ eventosCima.js
@@ -1,53 +1,17 @@
-//Declaración de elementos de contenido de inicio por ID
-let contenidoDeAlojamientos = document.getElementById('contenidoDeAlojamientos');
-let contenidoDeServicios = document.getElementById('contenidoDeServicios');
-let contenidoDeUbicacion = document.getElementById('contenidoDeUbicacion');
-let contenidoDeConocenos = document.getElementById('contenidoDeConocenos');
-
-//Ocultar contenidos de inicio
-contenidoDeServicios.style.display = 'none';
-contenidoDeUbicacion.style.display = 'none';
-contenidoDeConocenos.style.display = 'none';
-
-//Arrays de ID
-let idBotonesDeNav = ['navAlojamientos', 'navServicios', 'navUbicacion', 'navConocenos'];
-let idBotonesDeMiniaturas = [
-    'botonMasDetallesCab1', 'botonReservarCab1', 'botonMasDetallesCab2', 'botonReservarCab2', 
-    'botonMasDetallesCab3', 'botonReservarCab3', 'botonMasDetallesCab4', 'botonReservarCab4', 
-    'botonMasDetallesCab5', 'botonReservarCab5'
-];
-
-//Arrays vacios para recibir elementos por ID
-let elementosDeNav = [];
-let elementosBotonesMiniaturas = [];
-
-//Generación de Array con IDs
-//  Elementos de nav
-idBotonesDeNav.forEach(function(boton){
-    elementosDeNav.push(document.getElementById(boton));
-});
-//  Elementos de botones de miniaturas
-idBotonesDeMiniaturas.forEach(function(boton){
-    elementosBotonesMiniaturas.push(document.getElementById(boton));
-});
-
-//Event handlers de los botones de index.html
-elementosDeNav.forEach(botonesNav)
-elementosBotonesMiniaturas.forEach(botonesMiniaturas);
-
-//Funciones para eventos sobre botones
-//  Botones Nav
+//Array de IDs de elementos de los distintos contenidos
+let contenidoId = ['contenidoDeAlojamientos', 'contenidoDeServicios', 'contenidoDeUbicacion', 'contenidoDeConocenos'].map(id => document.getElementById(id));
+//Array de IDs de elementos de Nav
+let botonesNavId = ['navAlojamientos', 'navServicios', 'navUbicacion', 'navConocenos'].map(id => document.getElementById(id));
+//Caracteristica default para que al principio solo el contenido de Laojamientos sea visible
+contenidoId[1].style.display = 'none';
+contenidoId[2].style.display = 'none';
+contenidoId[3].style.display = 'none';
+//Funcion para cambiar el z-index de los botones nav y para cambiar la propiedad display de los contenidos ligados a cada boton
 function botonesNav(boton) {
     boton.onclick = function() {
-        navAlojamientos.style.zIndex = 0;
-        navServicios.style.zIndex = 0;
-        navUbicacion.style.zIndex = 0;
-        navConocenos.style.zIndex = 0;
+        botonesNavId.forEach(elemento => elemento.style.zIndex = 0);
         event.target.style.zIndex = 2;
-        contenidoDeAlojamientos.style.display = 'none';
-        contenidoDeServicios.style.display = 'none';
-        contenidoDeUbicacion.style.display = 'none';
-        contenidoDeConocenos.style.display = 'none';
+        contenidoId.forEach(elemento => elemento.style.display = 'none');
         switch (event.target.id) {
             case 'navAlojamientos':
                 contenidoDeAlojamientos.style.display = '';
@@ -64,23 +28,7 @@ function botonesNav(boton) {
         }
     }
 }
-//  Botones de miniaturas
-function botonesMiniaturas(boton){
-    boton.onmouseenter = function() {
-        event.target.style.backgroundColor = 'hsl(0, 0%, 95%)';
-        event.target.style.border = 'hsl(0, 0%, 75%) 2px solid';
-    }
-    boton.onmouseleave = function() {
-        event.target.style.backgroundColor = '';
-        event.target.style.border = '';
-    }
-    boton.onmousedown = function() {
-        event.target.style.backgroundColor = 'rgb(207,215,191)';
-        event.target.style.border = 'hsl(0, 0%, 75%) 5px solid';
-    }
-    boton.onmouseup = function() {
-        event.target.style.backgroundColor = '';
-        event.target.style.border = '';
-    }
-}
+botonesNavId.forEach(botonesNav);
+
+
 
