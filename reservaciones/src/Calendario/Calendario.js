a3f2b346bf4a4fc23eb1e0203cb0a32f44489f4e
José Carlos Cárdenas Martínez <Josecm@icloud.com>
2020-06-09 21:22:32 -0500

    Cambio de meses con botones
    El calendario ahora puede retroceder y avanzar los meses sin perder la logica de los años

--- reservaciones/src/Calendario/Calendario.js
+++ reservaciones/src/Calendario/Calendario.js
@@ -1,75 +1,77 @@
 import React from 'react';
 import './Calendario.css';
-import FechaCal from '../FechaCal/FechaCal';
+import Mes from '../Mes/Mes';
+import botonArribaImg from './botonArriba.svg'
+import botonAbajoImg from './botonAbajo.svg'
 
 
 class Calendario extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            meses: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-            fechaActual: new Date(Date.now())
-        }
-        this.mesActual = this.mesActual.bind(this);
-        this.renderizarDias = this.renderizarDias.bind(this);
+    constructor(props) {
+      super(props);
+      this.state = {
+        mesVisible: new Date(Date.now()).getMonth(),
+        anioVisible: new Date(Date.now()).getFullYear()
+      }
+      this.segundoMesVisible = this.segundoMesVisible.bind(this);
+      this.segundoAnioVisible = this.segundoAnioVisible.bind(this);
+      this.mesAnterior = this.mesAnterior.bind(this);
+      this.mesSiguiente = this.mesSiguiente.bind(this);
     }
-    mesActual() {
-        // Regresa el nombre del mes actual
-        let mesNumerico = this.props.mes;
-        return this.state.meses[mesNumerico];
+    segundoMesVisible() {
+      if(this.state.mesVisible === 11){
+        return 0;
+      } else {
+        return this.state.mesVisible + 1;
+      }
     }
-    renderizarDias() {
-        const mesActualNumerico = this.props.mes;
-        const anioActual = this.state.fechaActual.getFullYear();
-        let primerDia = new Date(anioActual, mesActualNumerico, 1);
-        // Normalmante getDay() inicia en domingo, con este operador ternario iniciamos en lunes con valor en 1 hasta domingo con valor 7
-        (primerDia.getDay() === 0)? primerDia = 6 : primerDia = primerDia.getDay() - 1;
-        const diasDelMes = (anio, mes) => {
-            return new Date(anio, mes, 0).getDate();
-        }
-        const semanas = [];
-        const filaSemana = (numeroDeFila) => {
-            let diasTranscurridos = numeroDeFila * 7;
-            const fila = [];
-            for (let i = 1; i <= 7; i++){
-                if(diasTranscurridos + i <= diasDelMes(anioActual, mesActualNumerico + 1) + primerDia){
-                    if(i <= primerDia && numeroDeFila == 0){
-                        fila.push(<td></td>)
-                    } else {
-                        fila.push(<td><FechaCal diaDelMes={diasTranscurridos + i - primerDia}/></td>);
-                    }
-                }
-            }
-            return fila;
-        }
-        for(let i = 0; i < 6; i++){
-            semanas.push(<tr>{filaSemana(i)}</tr>)
-        }
-        return semanas;
+    segundoAnioVisible() {
+      if(this.state.mesVisible === 11){
+        return this.state.anioVisible + 1;
+      } else {
+        return this.state.anioVisible;
+      }
     }
-    render(){
-        return (
-            <div>
-                <table>
-                    <thead>
-                        <th colSpan = '7'>{this.mesActual()} {this.props.anio}</th>
-                        <tr>
-                            <td>Lun</td>
-                            <td>Mar</td>
-                            <td>Mie</td>
-                            <td>Jue</td>
-                            <td>Vie</td>
-                            <td>Sab</td>
-                            <td>Dom</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderizarDias()}
-                    </tbody>
-                </table>
-            </div>
-        );
+    mesAnterior() {
+        let mes;
+        let anio;
+        if(this.state.mesVisible === 0){
+            mes = 11;
+            anio = this.state.anioVisible - 1;
+            this.setState({
+                mesVisible: mes,
+                anioVisible: anio
+            })
+        } else {
+            mes = this.state.mesVisible - 1;
+            this.setState({mesVisible: mes});
+        }
+    }
+    mesSiguiente() {
+        let mes;
+        let anio;
+        if(this.state.mesVisible === 11){
+            mes = 0;
+            anio = this.state.anioVisible + 1;
+            this.setState({
+                mesVisible: mes,
+                anioVisible: anio
+            })
+        } else {
+            mes = this.state.mesVisible + 1;
+            this.setState({mesVisible: mes});
+        }
+    }
+    render() {
+      return (
+        <div className="App">
+            <button onClick={this.mesAnterior}><img className='botones' src={botonArribaImg}/></button>
+            <Mes mes={this.state.mesVisible} anio={this.state.anioVisible}/>
+            <Mes mes={this.segundoMesVisible()} anio={this.segundoAnioVisible()}/>
+            <button onClick={this.mesSiguiente}><img className='botones' src={botonAbajoImg}/></button>
+        </div>
+      );
     }
 }
+
 
 export default Calendario;
---
+++ reservaciones/src/Mes/Mes.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './Mes.css';
+import FechaCal from '../FechaCal/FechaCal';
+
+
+
+class Mes extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            meses: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            fechaActual: new Date(Date.now())
+        }
+        this.mesActual = this.mesActual.bind(this);
+        this.renderizarDias = this.renderizarDias.bind(this);
+    }
+    mesActual() {
+        // Regresa el nombre del mes actual
+        let mesNumerico = this.props.mes;
+        return this.state.meses[mesNumerico];
+    }
+    renderizarDias() {
+        const mesActualNumerico = this.props.mes;
+        const anioActual = this.state.fechaActual.getFullYear();
+        let primerDia = new Date(anioActual, mesActualNumerico, 1);
+        // Normalmante getDay() inicia en domingo, con este operador ternario iniciamos en lunes con valor en 1 hasta domingo con valor 7
+        (primerDia.getDay() === 0)? primerDia = 6 : primerDia = primerDia.getDay() - 1;
+        const diasDelMes = (anio, mes) => {
+            return new Date(anio, mes, 0).getDate();
+        }
+        const semanas = [];
+        const filaSemana = (numeroDeFila) => {
+            let diasTranscurridos = numeroDeFila * 7;
+            const fila = [];
+            for (let i = 1; i <= 7; i++){
+                if(diasTranscurridos + i <= diasDelMes(anioActual, mesActualNumerico + 1) + primerDia){
+                    if(i <= primerDia && numeroDeFila == 0){
+                        fila.push(<td></td>)
+                    } else {
+                        fila.push(<td><FechaCal diaDelMes={diasTranscurridos + i - primerDia}/></td>);
+                    }
+                }
+            }
+            return fila;
+        }
+        for(let i = 0; i < 6; i++){
+            semanas.push(<tr>{filaSemana(i)}</tr>)
+        }
+        return semanas;
+    }
+    render(){
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <th colSpan = '7'>{this.mesActual()} {this.props.anio}</th>
+                        <tr>
+                            <td>Lun</td>
+                            <td>Mar</td>
+                            <td>Mie</td>
+                            <td>Jue</td>
+                            <td>Vie</td>
+                            <td>Sab</td>
+                            <td>Dom</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderizarDias()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default Mes;
